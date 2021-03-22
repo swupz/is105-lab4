b19058bc0e8ec6603a8b7be558bf2daf15d612a0
Daniel S. Revne <danielsrevne@hotmail.no>
2021-03-22 12:13:01 +0100

    Kompilerte filene og gjorde alt klart til godkjenning

--- fork.c
+++ fork.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
 int main() {
    fork();
    fork();
--- writefile.c
+++ writefile.c
@@ -1,14 +1,18 @@
 #include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
-    int fd, sz;
+    int fd, sz, sx;
 
     fd = open("bar.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (fd < 0) {
         perror("feil ved skriving av filen"); exit(1);
     }
-    sz = write(fd, "Hello\0", 6);
+    sz = write(fd, "Hello you\0", 10);
+  //  sx = write(fd, "Alphabet\0", 8);
 
     close(fd);
 }
